a7c8c6dbc2883fc2ccba807bbadc4d722cde7a91
Supa <36031171+0Supa@users.noreply.github.com>
2025-08-12 05:20:13 +0300

    update flake

--- flake.nix
+++ flake.nix
@@ -8,12 +8,12 @@
   outputs =
     { self, nixpkgs }:
     let
-      module = "uploader-basic";
+      pname = "uploader-basic";
       user = "uploader-basic";
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       goServer = pkgs.buildGoModule {
-        pname = "uploader-basic";
+        pname = pname;
         version = "git-${self.rev or "dirty"}";
         src = ./.;
         vendorHash = null;
@@ -22,7 +22,7 @@
     {
       packages.${system}.default = goServer;
 
-      nixosModules.${module} =
+      nixosModules.default =
         { config, pkgs, ... }:
         {
           users.users.${user} = {
@@ -31,12 +31,12 @@
             group = "www";
           };
 
-          systemd.services.${module} = {
+          systemd.services.${pname} = {
             description = "Simple Go HTTP server";
             after = [ "network.target" ];
             wantedBy = [ "multi-user.target" ];
             serviceConfig = {
-              ExecStart = "${goServer}/bin/${module}";
+              ExecStart = "${goServer}/bin/${pname}";
               Restart = "always";
               User = user;
               WorkingDirectory = "/var/lib/${user}";
